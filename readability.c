72efa28e130ca571b7f9e3d73849bf1a0ad37e87
Srikar721-EDX <Srikar721-EDX@users.noreply.github.com>
2020-08-02 20:58:14 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -12,20 +12,29 @@ int main(void)
     for (int i = 0, len = strlen(s); i < len; i++)
     {
         if (isalpha(s[i]))
+        
             num_letters++;
+            
         if ((i == 0 && s[i] != ' ') 
             || (i != len - 1 && s[i] == ' ' && s[i + 1] != ' '))
+            
             num_words++;
+            
         if (s[i] == '.' || s[i] == '?' || s[i] == '!')
+        
             num_sentences++;
     }
     float L = (num_letters / (float) num_words) * 100;
     float S = (num_sentences / (float) num_words) * 100;
     int index = round(0.0588 * L - 0.296 * S - 15.8);
     if (index < 1)
+    
         printf("Before Grade 1\n");
+        
     else if (index >= 16)
+    
         printf("Grade 16+\n");
     else
+    
         printf("Grade %i\n", index);
 }
