b1c08e9e12107b09fec6f622395faf40c27f4fb9
Srikar721-EDX <Srikar721-EDX@users.noreply.github.com>
2020-08-08 18:46:32 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,8 +1,21 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int row=0; row<height; row++)
+    {
+        for(int col=0; col<width; col++)
+        {
+            float average = (image[row][col].rgbtBlue + image[row][col].rgbtRed + image[row][col].rgbtGreen)/3;
+            int avg = round(average);
+            
+            image[row][col].rgbtBlue = avg;
+            image[row][col].rgbtRed = avg;
+            image[row][col].rgbtGreen = avg;
+        }
+    }
     return;
 }
 
